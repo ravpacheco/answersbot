01bf36050934d902866bd9de74609952fb94b838
Rafael Avelar Pacheco <rafaelpa@takenet.com.br>
2016-08-25 18:11:08 -0300

    Create a initial structure to receive messages and notifications

---
+++ src/answersbot/Controllers/MessagesController.cs
@@ -0,0 +1,27 @@
+﻿using Lime.Protocol;
+using Lime.Protocol.Serialization;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace answersbot.Controllers
+{
+    public class MessagesController : ApiController
+    {
+
+        // POST api/values
+        public void Post(JObject jsonObject)
+        {
+            var envelopeSerializer = new EnvelopeSerializer();
+            
+            var message = envelopeSerializer.Deserialize(jsonObject.ToString());
+
+            Console.WriteLine("Received Message");
+        }
+
+    }
+}
