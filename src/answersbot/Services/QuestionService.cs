e70a4061115ba410ca87e10a66249f6d4dfc094a
Paulo Henrique Abreu <pauloh@takenet.com.br>
2016-08-26 09:47:40 -0300

    create questionservice

---
+++ src/answersbot/Services/QuestionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using answersbot.Models;
+using answersbot.Storage;
+
+namespace answersbot.Services
+{
+    public class QuestionService
+    {
+        public async Task<Question> GetRandomQuestion(User user)
+        {
+            // Get a Question that
+            // 1. It is not mine question
+            var database = DataContext.Database();
+            database
+
+
+            // 2. It is not a question that was answered by me
+
+        }
+    }
+}
